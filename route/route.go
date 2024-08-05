01f4054ff69cf85c0fee80cdccf2b14412a476cf
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-08-05 15:41:04 +0100

    added the deleteBook and updateBook api

--- route/route.go
+++ route/route.go
@@ -1,9 +1,9 @@
 package route
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/stanleychukwu17/golang-gorm-and-postgres-bookstore-api/schema"
 
@@ -22,7 +22,6 @@ func (r *Repository) CreateBook(context *fiber.Ctx) error {
 
 	// Parse the request body & bind it to the book struct
 	// the context.BodyParser(&book), is parsing the request body from json to go struct.. Fiber does this internally, be default, Golang does not understand json
-	fmt.Println(context.Body())
 	if err := context.BodyParser(&book); err != nil {
 		// Log the error and respond with a 400 Bad Request
 		log.Println("Error parsing request body:", err)
@@ -55,11 +54,66 @@ func (r *Repository) CreateBook(context *fiber.Ctx) error {
 	return nil
 }
 
+// deleting a book
+func (r *Repository) DeleteBook(context *fiber.Ctx) error {
+	idStr := context.Params("id")
+	id, err := strconv.ParseUint(idStr, 10, 16)
+	if err != nil {
+		return context.Status(http.StatusBadRequest).JSON(map[string]interface{}{
+			"message": "Invalid ID received",
+		})
+	}
+
+	// Execute a raw SQL DELETE query
+	result := r.DB.Exec("DELETE FROM books WHERE id = ?", id)
+	if result.Error != nil {
+		return context.Status(http.StatusNotModified).JSON(map[string]interface{}{
+			"message": result.Error,
+		})
+	}
+
+	// return a success message
+	return context.Status(http.StatusOK).JSON(map[string]interface{}{
+		"message": "item deleted successfully",
+	})
+}
+
+func (r *Repository) updateBook(context *fiber.Ctx) error {
+	book_id := context.Params("id")
+	book := schema.Book{}
+
+	// check if the book record exists
+	if err := r.DB.First(&book, book_id).Error; err != nil {
+		return context.Status(http.StatusNotFound).JSON(map[string]interface{}{
+			"message": "Book not found",
+		})
+	}
+
+	// Parse the request body
+	if err := context.BodyParser(&book); err != nil {
+		return context.Status(http.StatusBadRequest).JSON(map[string]interface{}{
+			"message": err.Error(),
+		})
+	}
+
+	// update the book
+	if err := r.DB.Save(&book).Error; err != nil {
+		return context.Status(fiber.StatusInternalServerError).JSON(map[string]interface{}{
+			"message": "Failed to update user",
+		})
+	}
+
+	return context.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Book updated successfully",
+	})
+}
+
 // sets up all the routes
 func (r *Repository) SetupRoutes(app *fiber.App) {
 	api := app.Group("/api")
 	api.Post("/createBook", r.CreateBook)
-	// api.delete("/deleteBook/:id", r.DeleteBook)
+	api.Delete("/deleteBook/:id", r.DeleteBook)
+	api.Patch("/updateBook/:id", r.updateBook)
 	// api.get("/getBook/:id", r.GetBookByID)
 	// api.get("/allBooks", r.GetAllBooks)
 }
