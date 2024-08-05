eb66f2a68e764265f4f0485b494bd593215930da
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-08-05 12:16:08 +0100

    connected to the db migrated the table setup route handler

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"log"
 	"os"
 
+	"github.com/stanleychukwu17/golang-gorm-and-postgres-bookstore-api/route"
+	"github.com/stanleychukwu17/golang-gorm-and-postgres-bookstore-api/schema"
+	"github.com/stanleychukwu17/golang-gorm-and-postgres-bookstore-api/storage"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 )
@@ -21,10 +25,12 @@ func main() {
 		log.Fatalf("PORT environment variable is required but not set")
 	}
 
-	// db, err := storage.NewConnection(config)
-	// if err != nil {
-	// 	log.Fatalf("Error connecting to database: %v", err)
-	// }
+	db, err := storage.NewConnection()
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	} else {
+		fmt.Println("Connected to database", db)
+	}
 
 	// Create new Fiber instance
 	app := fiber.New()
@@ -33,6 +39,19 @@ func main() {
 	app.Get("/", func(context *fiber.Ctx) error {
 		return context.SendString("Hello, World!")
 	})
+
+	// set_up a new Repository
+	r := &route.Repository{
+		DB: db,
+	}
+
+	// Automatically migrate your schema
+	err = db.AutoMigrate(&schema.User{}, &schema.Book{})
+	if err != nil {
+		log.Fatalf("failed to migrate database: %v", err)
+	}
+
+	r.SetupRoutes(app)
 
 	// Start server
 	app.Listen(fmt.Sprintf(":%s", port))
